d293f2a3d56e2008d218d87557bc40857c351d2a
shuva123 <33460198+shuva123@users.noreply.github.com>
2019-07-11 16:56:24 +0530

    Add files via upload

---
+++ SimpleCalculator/ICalulator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ICalulator
+    {
+
+        [OperationContract]
+         double AddNumber(double num1, double num2);
+
+
+        [OperationContract]
+         double SubNumber(double num1, double num2);
+        
+
+
+    }
+}
